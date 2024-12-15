9f9620686ff12bbd95f24d5cbbdc05ffd854a589
vkitise <151126627+vkitise@users.noreply.github.com>
2024-12-15 23:37:15 +0530

    Update 4.c

--- 4.c
+++ 4.c
@@ -1,71 +1,56 @@
-#include <ctype.h>
 #include <stdio.h>
-#define size 50 /* size of stack */
-char s[size];
-int top = -1; 
-void push(char elem) 
-{
-s[++top] = elem;
+#include <ctype.h>
+#define SIZE 50 
+char stack[SIZE];
+int top = -1;
+void push(char elem) {
+    stack[++top] = elem;
 }
-char pop() 
-{
-return s[top--];
+char pop() {
+    return stack[top--];
 }
-int pr(char elem) 
-{
-switch (elem)
-{
-case '#':
-return 0;
-case '(':
-return 1;
-case '+':
-case '-':
-return 2;
-case '*':
-case '/':
-case '%':
-return 3;
-case '^':
-return 4;
-default:
-return -1; 
+int precedence(char elem) {
+    switch (elem) {
+        case '#': return 0;
+        case '(': return 1;
+        case '+':
+        case '-': return 2;
+        case '*':
+        case '/':
+        case '%': return 3;
+        case '^': return 4;
+        default: return -1;
+    }
 }
-}
-void main() 
-{
-char infx[50], pofx[50], ch, elem;
-int i = 0, k = 0;
-printf("\n\nEnter the infix expression: ");
-scanf("%s", infx);
-push('#');
-while ((ch = infx[i++]) != '\0')
-{
-if (ch == '(')
-push(ch);
-else if (isalnum(ch))
-pofx[k++] = ch;
-else if (ch == ')')
-{
-while (s[top] != '(')
-{
-pofx[k++] = pop();
-}
-elem = pop(); 
-else 
-}
-{
-while (pr(s[top]) >= pr(ch))
-{
-pofx[k++] = pop();
-}
-push(ch);
-}
-}
-while (s[top] != '#') 
-{
-pofx[k++] = pop();
-}
-pofx[k] = '\0'; 
-printf("\n\n Given infix expression: %s\n postfix expression is: %s\n", infx, pofx);
+int main() {
+    char infix[50], postfix[50], ch, elem;
+    int i = 0, k = 0;
+
+    printf("\nEnter the infix expression: ");
+    scanf("%s", infix);
+
+    push('#'); 
+    while ((ch = infix[i++]) != '\0') {
+        if (ch == '(') {
+            push(ch); 
+        } else if (isalnum(ch)) {
+            postfix[k++] = ch; 
+        } else if (ch == ')') {
+            while (stack[top] != '(') {
+                postfix[k++] = pop();
+            }
+            pop(); 
+        } else {
+            while (precedence(stack[top]) >= precedence(ch)) {
+                postfix[k++] = pop();
+            }
+            push(ch);
+        }
+    }
+    while (stack[top] != '#') {
+        postfix[k++] = pop();
+    }
+    postfix[k] = '\0'; 
+    printf("\nGiven infix expression: %s\nPostfix expression: %s\n", infix, postfix);
+    return 0;
 }
