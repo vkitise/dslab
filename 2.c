432b7e947cbd9d8d9311c08801f144dab6aecd8d
Deekshith004 <151126627+Deekshith004@users.noreply.github.com>
2024-12-15 16:02:20 +0530

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,36 +1,37 @@
 #include <stdio.h>
-#include <string.h>
 int main() {
-    char STR[100], PAT[100], REP[100], result[100];
-    int i = 0, j = 0, k = 0, match = 0;
-    printf("Enter the MAIN string: ");
-    scanf(" %[^\n]", STR);
-    printf("Enter the PATTERN string: ");
-    scanf("%s", PAT);
-    printf("Enter the REPLACE string: ");
-    scanf("%s", REP);
-    while (STR[i] != '\0') {
-        match = 1;
-        for (k = 0; PAT[k] != '\0'; k++) {
-            if (STR[i + k] != PAT[k]) {
-                match = 0;
-                break;
-            }
-        }
-        if (match) {
-            for (k = 0; REP[k] != '\0'; k++) {
-                result[j++] = REP[k];
-            }
-            i += strlen(PAT); 
-        } else {
-            result[j++] = STR[i++];
-        }
-    }
-    result[j] = '\0'; 
-    if (j > 0) {
-        printf("\nThe RESULTANT string is: %s\n", result);
-    } else {
-        printf("Pattern not found!!!\n");
-    }
-    return 0;
+char STR[100], PAT[100], REP[100], ans[100];
+int i, j, c, m, k, flag = 0;
+printf("\nEnter the MAIN string: ");
+scanf("%99[^\n]", STR); 
+printf("Enter a PATTERN string: ");
+scanf("%99s", PAT);
+printf("Enter a REPLACE string: ");
+scanf("%99s", REP);
+i = m = c = j = 0;
+while (STR[c] != '\0') {
+if (STR[c] == PAT[i]) {
+i++;
+c++;
+flag = 1;
+if (PAT[i] == '\0') {
+for (k = 0; REP[k] != '\0'; k++, j++)
+ans[j] = REP[k];
+i = 0; 
+}
+} else { 
+ans[j] = STR[c];
+j++;
+c++;
+m = c;
+i = 0; 
+}
+}
+if (flag == 0) {
+printf("Pattern doesn't found!!!\n");
+} else {
+ans[j] = '\0';
+printf("\nThe RESULTANT string is: %s\n", ans);
+}
+return 0;
 }
