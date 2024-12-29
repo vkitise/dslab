4669716d5233d0e304b060a187d660a22194803c
vkitise <151126627+vkitise@users.noreply.github.com>
2024-12-29 23:18:33 +0530

    Update 4.c

--- 4.c
+++ 4.c
@@ -13,44 +13,37 @@ int precedence(char elem) {
     switch (elem) {
         case '#': return 0;
         case '(': return 1;
-        case '+':
-        case '-': return 2;
-        case '*':
-        case '/':
-        case '%': return 3;
+        case '+': case '-': return 2;
+        case '*': case '/': case '%': return 3;
         case '^': return 4;
         default: return -1;
     }
 }
 int main() {
-    char infix[50], postfix[50], ch, elem;
+    char infix[SIZE], postfix[SIZE];
     int i = 0, k = 0;
-
-    printf("\nEnter the infix expression: ");
+    char ch;
+    printf("Enter the infix expression: ");
     scanf("%s", infix);
-
     push('#'); 
     while ((ch = infix[i++]) != '\0') {
-        if (ch == '(') {
-            push(ch); 
-        } else if (isalnum(ch)) {
+        if (isalnum(ch)) {
             postfix[k++] = ch; 
+        } else if (ch == '(') {
+            push(ch);
         } else if (ch == ')') {
-            while (stack[top] != '(') {
+            while (stack[top] != '(') 
                 postfix[k++] = pop();
-            }
-            pop(); 
+            pop(); // Remove '(' from stack
         } else {
-            while (precedence(stack[top]) >= precedence(ch)) {
+            while (precedence(stack[top]) >= precedence(ch)) 
                 postfix[k++] = pop();
-            }
             push(ch);
         }
     }
-    while (stack[top] != '#') {
+    while (stack[top] != '#') 
         postfix[k++] = pop();
-    }
     postfix[k] = '\0'; 
-    printf("\nGiven infix expression: %s\nPostfix expression: %s\n", infix, postfix);
+    printf("\nInfix expression: %s\nPostfix expression: %s\n", infix, postfix);
     return 0;
 }
